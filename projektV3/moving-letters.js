f3a53362a2b2bf41e08b2b54d263112b7776ead4
arwenbae99 <151166379+arwenbae99@users.noreply.github.com>
2024-04-08 16:37:00 +0200

    Add files via upload

---
+++ projektV3/moving-letters.js
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////// Mapping Elements
+function mapRange(value, oldMin, oldMax, newMin, newMax) {
+    // Check if the value is within the old range
+    if (value < oldMin || value > oldMax) {
+        throw new Error("Value is not within the old range");
+    }
+    // Calculate the percentage of the value within the old range
+    const percentage = (value - oldMin) / (oldMax - oldMin);
+
+    // Map the percentage to the new range
+    const newValue = percentage * (newMax - newMin) + newMin;
+
+    return newValue;
+}
+
+let Movement;
+
+
+document.addEventListener("mousemove", function (event) {
+    const mouseX = event.clientX;
+    const centerOfScreenX = window.innerWidth * 0.5;
+
+
+
+    if (mouseX < centerOfScreenX) {
+        Movement = mapRange(mouseX, 0, centerOfScreenX, 200, 0);
+    } else {
+        Movement = (mapRange(mouseX, centerOfScreenX, window.innerWidth, 0, 200) * -1);
+    }
+
+    // Update Location
+    document.documentElement.style.setProperty("--movement", Movement + "px");
+    console.log("--movement", Movement + "px");
+});
